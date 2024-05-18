b54d2f69566ced900ec123e421247a23e92edb3e
fishfoldyy <145004951+fishfoldyy@users.noreply.github.com>
2024-05-18 20:24:35 +0800

    Create shortcuts.js

---
+++ archive/shortcuts/shortcuts.js
@@ -0,0 +1,51 @@
+// home
+
+document.getElementById("home").addEventListener("click", function() {
+    window.location.href = "/site/archive";
+});
+
+// social media
+
+document.getElementById("facebook").addEventListener("click", function() {
+    window.location.href = "https://facebook.com";
+});
+
+document.getElementById("x").addEventListener("click", function() {
+    window.location.href = "https://twitter.com";
+});
+
+document.getElementById("tiktok").addEventListener("click", function() {
+    window.location.href = "https://www.tiktok.com";
+});
+
+// gaming
+
+document.getElementById("shellshock").addEventListener("click", function() {
+    window.location.href = "https://shellshock.io";
+});
+
+document.getElementById("eagler").addEventListener("click", function() {
+    window.location.href = "https://eaglercraft.com/mc/1.8.8/";
+});
+
+document.getElementById("neal.fun").addEventListener("click", function() {
+    window.location.href = "https://neal.fun";
+});
+
+document.getElementById("wordle").addEventListener("click", function() {
+    window.location.href = "https://www.nytimes.com/games/wordle/index.html";
+});
+
+// programming
+
+document.getElementById("scratch").addEventListener("click", function() {
+    window.location.href = "https://scratch.mit.edu";
+});
+
+document.getElementById("replit").addEventListener("click", function() {
+    window.location.href = "https://replit.com";
+});
+
+document.getElementById("github").addEventListener("click", function() {
+    window.location.href = "https://github.com";
+});
